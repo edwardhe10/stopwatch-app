b1f681650377763ef6415d6f59546808a701f3b0
edwardhe10 <88951771+edwardhe10@users.noreply.github.com>
2023-07-16 02:54:01 -0400

    functionality done with simple styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Stopwatch from "./components/Stopwatch.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="stopwatch-app">
+      <Stopwatch />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Stopwatch() {
-  return <div>Stopwatch</div>;
+  const [time, setTime] = useState(0);
+
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalID;
+    if (running) {
+      // increment time every 10ms
+      intervalID = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalID); // cleanup
+  }, [running, time]); // trigger when running or time changes
+
+  // time calculations for hrs, mins, secs, millisecs
+  const hrs = Math.floor(time / 360000);
+  const mins = Math.floor((time % 360000) / 6000);
+  const secs = Math.floor((time % 6000) / 100);
+  const millisecs = time % 100;
+
+  const runTimer = () => {
+    setRunning(!running);
+  };
+
+  const resetTimer = () => {
+    setTime(0);
+  };
+
+  return (
+    <div>
+      <p className="stopwatch-timer">
+        {hrs}:{mins.toString().padStart(2, "0")}:
+        {secs.toString().padStart(2, "0")}:
+        {millisecs.toString().padStart(2, "0")}
+      </p>
+      <div className="buttons">
+        <button className="run-button" onClick={runTimer}>
+          {running ? "Stop" : "Start"}{" "}
+        </button>
+        <button className="reset-button" onClick={resetTimer}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Stopwatch;
